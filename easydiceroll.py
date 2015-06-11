3d0d3b796275b8ab07e8b499ba34cc8a314cb509
Simon AM <simon@agius-muscat.net>
2015-06-11 23:22:21 +0100

    Initial commit

---
+++ easydiceroll.py
@@ -0,0 +1,18 @@
+print "Welcome to easy-dice-roll!"
+print "Made by Winter259/Simon AM"
+print "You can fork me on github!"
+print "https://github.com/Winter259/easy-dice-roll"
+
+def choose_dice_type():
+	dice_type = raw_input("Please enter the kind of dice you will be using: ")
+
+def display_menu():
+	print "MENU:"
+	print "1: Roll Dice"
+	print "2: Choose Dice Type"
+	print "3: Exit"
+	
+while true:
+	
+	
+	
