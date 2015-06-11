114a46538b88353d014b029c59e6f5a285f56cee
Simon AM <simon@agius-muscat.net>
2015-06-11 23:27:13 +0100

    Added menu and choice

--- easydiceroll.py
+++ easydiceroll.py
@@ -7,12 +7,24 @@ def choose_dice_type():
 	dice_type = raw_input("Please enter the kind of dice you will be using: ")
 
 def display_menu():
-	print "MENU:"
-	print "1: Roll Dice"
-	print "2: Choose Dice Type"
-	print "3: Exit"
+	input = 0
+	while input < 0 || input > 3:
+		print "MENU:"
+		print "1: Roll Dice"
+		print "2: Choose Dice Type"
+		print "3: Exit"
+		input = raw_input("Please enter your choice:")
+		menu_choice(input)
+		
+def menu_choice(input):
+	if input == 1:
+		# roll dice!
+	elif input == 2:
+		# choose dice type!
+	elif: input == 3:
+		# exit!
+	else:
+		print "ERROR: Input out of bounds!"
 	
 while true:
-	
-	
-	
+	display_menu()
