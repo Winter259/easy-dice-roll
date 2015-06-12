76ff7004162b98cd6c9658bb419b97a876ef2c02
Simon AM <simon@agius-muscat.net>
2015-06-12 11:29:01 +0200

    Added basic dice changing

--- dice_fnc.py
+++ dice_fnc.py
@@ -1,21 +1,26 @@
 __author__ = 'Simon'
 
+import random
+
 dice_types = [4,6,8,10,12,20,20,100]
 
 def choose_dice_type():
     print("Current Dice types available: ")
     for dice in dice_types:
-        print(dice), print(" "),
+        print(dice, end=' ')
+    print("\n")
     try:
-        dice_choice = int(input("Enter the number of sides the dice you wish to use has:"))
+        dice_choice = int(input("Enter the number of sides the dice you wish to use has: "))
     except ValueError:
         print("ERROR! Input out of bounds")
+        dice_choice = 0
     while not check_dice_type(dice_choice):
         try:
-            dice_choice = int(input("Enter the number of sides the dice you wish to use has:"))
+            dice_choice = int(input("Enter the number of sides the dice you wish to use has: "))
         except ValueError:
-            print("ERROR! Input out of bounds")
+            print("ERROR: Input out of bounds")
             dice_choice = 0 # stops double error messages
+    print("Now using: d{}".format(dice_choice))
     return dice_choice
 
 
@@ -25,3 +30,19 @@ def check_dice_type(chosen_dice):
         return False
     else:
         return True
+
+def check_int_bounds(integer_variable,lower_bound,upper_bound):
+    if integer_variable > upper_bound or integer_variable < lower_bound:
+        return False
+    else:
+        return True
+
+def check_integer_loop(integer,lower,upper):
+    while not check_int_bounds(integer,lower,upper):
+        print("ERROR: Input out of bounds!")
+        integer = int(input("Please enter a valid number: "))
+    return integer
+
+def roll_dice(current_dice):
+    roll_amount = int(input(("How many d% do you want to roll?") % current_dice))
+    roll_amount = check_integer_loop(roll_amount,0,100)
--- easydiceroll.py
+++ easydiceroll.py
@@ -1,15 +1,16 @@
 __author__ = 'Simon'
 
 from dice_fnc import *
+import os
+clear = lambda: os.system('cls')
 
 print("Welcome to easy-dice-roll!")
 print("You can fork me on github!")
 print("https://github.com/Winter259/easy-dice-roll")
 
-# init vars
-current_dice_type = 0
-
 def display_menu():
+	# clear()
+	print("Current dice being used: d{}".format(current_dice_type))
 	print("MENU:")
 	print("1: Roll Dice")
 	print("2: Choose Dice Type")
@@ -22,20 +23,22 @@ def menu_choice(choice):
 	elif choice == 2:
 		""" choose dice type! """
 		print("Lets change the dice!")
-		current_dice = choose_dice_type()
+		current_dice_type = choose_dice_type()
 	else:
 		""" do nothing! exit is handle by main"""
+
+# init vars
+current_dice_type = choose_dice_type()
 
 while True:
 	menu_input = -1 # stops choice looping
 	display_menu()
 	try:
 		menu_input = int(input("Please enter your choice: "))
-		menu_choice(menu_input)
+		current_dice_type = menu_choice(menu_input)
 	except ValueError:
 		print("ERROR: Input out of bounds!")
-	if menu_input > 3 or menu_input < 1:
-		print("ERROR: No Menu option available!")
+	check_integer_loop(menu_input,1,3)
 	if menu_input == 3:
 		print("Bye!")
 		break
