1e59c9377ad823940dd8fe3abf34f657f7306d28
Simon AM <simon@agius-muscat.net>
2015-06-12 10:22:50 +0200

    Finished menu

--- easydiceroll.py
+++ easydiceroll.py
@@ -1,5 +1,4 @@
 print("Welcome to easy-dice-roll!")
-print("Made by Winter259/Simon AM")
 print("You can fork me on github!")
 print("https://github.com/Winter259/easy-dice-roll")
 
@@ -7,27 +6,31 @@ def choose_dice_type():
 	dice_type = input("Please enter the kind of dice you will be using: ")
 
 def display_menu():
-	menu_input = -1
-	while menu_input < 0 or menu_input > 3:
 		print("MENU:")
 		print("1: Roll Dice")
 		print("2: Choose Dice Type")
 		print("3: Exit")
-		menu_input = int(input("Please enter your choice:"))
-		menu_choice(menu_input)
-		
-def menu_choice(menu_input):
-	if menu_input == 1:
+
+def menu_choice(choice):
+	if choice == 1:
 		""" roll dice! """
 		print("Lets roll a dice!")
-	elif menu_input == 2:
+	elif choice == 2:
 		print("Lets change the dice!")
 		""" choose dice type! """
-	elif menu_input == 3:
-		print("Lets get out of here!")
-		""" exit! """
 	else:
-		print("ERROR: Input out of bounds!")
-	
+		""" do nothing! exit is handle by main"""
+
 while True:
+	menu_input = -1
 	display_menu()
+	try:
+		menu_input = int(input("Please enter your choice: "))
+		menu_choice(menu_input)
+	except ValueError:
+		print("ERROR: Input out of bounds!")
+	if menu_input > 3 or menu_input < 1:
+		print("ERROR: No Menu option available!")
+	if menu_input == 3:
+		print("Bye!")
+		break
