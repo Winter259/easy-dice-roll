a2558c2e3c788f9966fe65028eef1eea3f521a30
Simon AM <simon@agius-muscat.net>
2015-06-12 10:43:17 +0200

    Converted dice to an object

--- easydiceroll.py
+++ easydiceroll.py
@@ -1,16 +1,19 @@
 __author__ = 'Simon'
 
 from dice_fnc import *
+from dice_classes import *
+
 import os
-clear = lambda: os.system('cls')
+def cls():
+	os.system(['clear','cls'][os.name == 'nt'])
 
 print("Welcome to easy-dice-roll!")
 print("You can fork me on github!")
 print("https://github.com/Winter259/easy-dice-roll")
 
 def display_menu():
-	# clear()
-	print("Current dice being used: d{}".format(current_dice_type))
+	# cls()
+	print("Current dice being used: d{}".format(my_dice.type))
 	print("MENU:")
 	print("1: Roll Dice")
 	print("2: Choose Dice Type")
@@ -23,19 +26,19 @@ def menu_choice(choice):
 	elif choice == 2:
 		""" choose dice type! """
 		print("Lets change the dice!")
-		current_dice_type = choose_dice_type()
+		my_dice.change_type()
 	else:
 		""" do nothing! exit is handle by main"""
 
-# init vars
-current_dice_type = choose_dice_type()
+# init dice
+my_dice = Dice()
 
 while True:
 	menu_input = -1 # stops choice looping
 	display_menu()
 	try:
 		menu_input = int(input("Please enter your choice: "))
-		current_dice_type = menu_choice(menu_input)
+		menu_choice(menu_input)
 	except ValueError:
 		print("ERROR: Input out of bounds!")
 	check_integer_loop(menu_input,1,3)
